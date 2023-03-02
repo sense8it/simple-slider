2601f78a741445899cddd19dc91c856660bcc466
sense8it <seoman2015y@gmail.com>
2023-03-02 14:02:37 +0200

    v 1.1 - added transition effect

--- script/main.js
+++ script/main.js
@@ -2,19 +2,19 @@ const arr = document.querySelectorAll("#gallery .photos img");
 let i = 0;
 
 document.querySelector(".next").onclick = function () {
-  arr[i].setAttribute("style", "display: none");
+  arr[i].setAttribute("class", " ");
   i++;
   if (i >= arr.length) {
     i = 0;
   }
-  arr[i].style.display = "inline-block";
+  arr[i].className = "showed";
 };
 
 document.querySelector("#gallery .buttons .prev").onclick = function () {
-  arr[i].setAttribute("style", "display: none");
+  arr[i].setAttribute("class", " ");
   i--;
   if (i < 0) {
     i = arr.length - 1;
   }
-  arr[i].style.display = "inline-block";
+  arr[i].className = "showed";
 };
