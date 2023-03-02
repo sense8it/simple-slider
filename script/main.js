8e76a460f9dcb92b6ec8b6d6dc17d11a63df5aea
sense8it <seoman2015y@gmail.com>
2023-03-02 15:37:14 +0200

    v 2.0 - added auto movements slides (setTimeout) - added new buttons stop and play - added new slides

--- script/main.js
+++ script/main.js
@@ -1,20 +1,80 @@
-const arr = document.querySelectorAll("#gallery .photos img");
+const arr = document.querySelectorAll('#gallery .photos img');
 let i = 0;
+// console.log(i);
+let activeButton= 'movement';
+document.querySelector('.play').disabled = true;
 
-document.querySelector(".next").onclick = function () {
-  arr[i].setAttribute("class", " ");
-  i++;
-  if (i >= arr.length) {
-    i = 0;
+const slider = function () {
+  switch (activeButton) {
+    case 'movement':
+      {
+        arr[i].setAttribute('class', ' ');
+        i++;
+
+        if (i < arr.length) {
+          arr[i].className = 'showed';
+          // console.log(i);
+          movementLoop();
+        } else {
+          i = 0;
+          arr[i].className = 'showed';
+          // console.log(i);
+          movementLoop();
+        }
+      }
+      break;
+    case 'click':
+      {
+        activeButton= 'movement';
+        movementLoop();
+      }
+      break;
+    case 'stop':
+      {
+        document.querySelector('.play').disabled = false;
+      }
+      break;
   }
-  arr[i].className = "showed";
 };
 
+function movementLoop() {
+  //timeout ms
+  setTimeout(slider, 4000);
+}
+movementLoop();
+
 document.querySelector("#gallery .buttons .prev").onclick = function () {
-  arr[i].setAttribute("class", " ");
+  activeButton= 'click';
+  arr[i].setAttribute("class", ' ');
   i--;
   if (i < 0) {
     i = arr.length - 1;
   }
+  // console.log(i);
   arr[i].className = "showed";
+};
+
+document.querySelector('.next').onclick = function () {
+  activeButton= 'click';
+  arr[i].setAttribute('class', ' ');
+  i++;
+  if (i >= arr.length) {
+    i = 0;
+  }
+  // console.log(i);
+  arr[i].className = 'showed';
+};
+
+document.querySelector('.stop').onclick = function () {
+  activeButton= 'stop';
+  this.disabled = true;
+  console.log('stop');
+};
+
+document.querySelector('.play').onclick = function () {
+  activeButton= 'movement';
+  this.disabled = true;
+  document.querySelector('.stop').disabled = false;
+  console.log('play');
+  movementLoop();
 };
