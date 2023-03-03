b91e1bfef608c5dfd2b48ac039a6cfa81c16a551
sense8it <seoman2015y@gmail.com>
2023-03-03 15:45:29 +0200

    v 2.1 - fixed stop bug, when the automatic sliding did not stop, after clicking on stop and at the same time repeatedly clicking on prev, next. - code optimization - added new slides

--- script/main.js
+++ script/main.js
@@ -1,80 +1,81 @@
-const arr = document.querySelectorAll('#gallery .photos img');
+const imgArr = document.querySelectorAll('#gallery .photos img');
 let i = 0;
-// console.log(i);
-let activeButton= 'movement';
+//x - transition effect timeout, ms
+let x = 4000;
+let activeButton = 'movement';
 document.querySelector('.play').disabled = true;
+console.log(i); //for debugging
 
-const slider = function () {
+const movementLoop = function() {
+  setTimeout(slider, x);
+}
+
+const slider = function() {
   switch (activeButton) {
     case 'movement':
       {
-        arr[i].setAttribute('class', ' ');
+        imgArr[i].classList.remove('showed');
         i++;
-
-        if (i < arr.length) {
-          arr[i].className = 'showed';
-          // console.log(i);
-          movementLoop();
-        } else {
+        if (i >= imgArr.length) {
           i = 0;
-          arr[i].className = 'showed';
-          // console.log(i);
-          movementLoop();
         }
+          imgArr[i].classList.add('showed');
+          console.log(i); //for debugging
+          movementLoop();
       }
       break;
     case 'click':
       {
-        activeButton= 'movement';
+        activeButton = 'movement';
         movementLoop();
       }
       break;
     case 'stop':
       {
         document.querySelector('.play').disabled = false;
+        document.querySelector('.next').disabled = false;
+        document.querySelector('.prev').disabled = false;
       }
       break;
   }
 };
 
-function movementLoop() {
-  //timeout ms
-  setTimeout(slider, 4000);
-}
 movementLoop();
 
 document.querySelector("#gallery .buttons .prev").onclick = function () {
-  activeButton= 'click';
-  arr[i].setAttribute("class", ' ');
+  activeButton = 'click';
+  imgArr[i].classList.remove('showed');
   i--;
   if (i < 0) {
-    i = arr.length - 1;
+    i = imgArr.length - 1;
   }
-  // console.log(i);
-  arr[i].className = "showed";
+  console.log(i); //for debugging
+  imgArr[i].classList.add('showed');
 };
 
 document.querySelector('.next').onclick = function () {
-  activeButton= 'click';
-  arr[i].setAttribute('class', ' ');
+  activeButton = 'click';
+  imgArr[i].classList.remove('showed');
   i++;
-  if (i >= arr.length) {
+  if (i >= imgArr.length) {
     i = 0;
   }
-  // console.log(i);
-  arr[i].className = 'showed';
+  console.log(i); //for debugging
+  imgArr[i].classList.add('showed');
 };
 
 document.querySelector('.stop').onclick = function () {
-  activeButton= 'stop';
+  activeButton = 'stop';
+  document.querySelector('.next').disabled = true;
+  document.querySelector('.prev').disabled = true;
   this.disabled = true;
-  console.log('stop');
+  console.log('stop'); //for debugging
 };
 
 document.querySelector('.play').onclick = function () {
-  activeButton= 'movement';
+  activeButton = 'movement';
   this.disabled = true;
   document.querySelector('.stop').disabled = false;
-  console.log('play');
+  console.log('play'); //for debugging
   movementLoop();
 };
