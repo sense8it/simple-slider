37fd8b5c54cd8d83183afc2a5797f4a38ac10520
sense8it <seoman2015y@gmail.com>
2023-03-04 18:27:59 +0200

    v 2.2 - removed a delay in which the buttons - prev and next were inactive after pressing stop - the prev and next buttons have been replaced with a checkbox - changed css - movement and movementLoop renamed in play and playLoop

--- script/main.js
+++ script/main.js
@@ -2,17 +2,16 @@ const imgArr = document.querySelectorAll('#gallery .photos img');
 let i = 0;
 //x - transition effect timeout, ms
 let x = 4000;
-let activeButton = 'movement';
-document.querySelector('.play').disabled = true;
-console.log(i); //for debugging
+let activeButton = 'play';
+// console.log(i); //for debugging
 
-const movementLoop = function() {
+const playLoop = function() {
   setTimeout(slider, x);
 }
 
 const slider = function() {
   switch (activeButton) {
-    case 'movement':
+    case 'play':
       {
         imgArr[i].classList.remove('showed');
         i++;
@@ -20,62 +19,61 @@ const slider = function() {
           i = 0;
         }
           imgArr[i].classList.add('showed');
-          console.log(i); //for debugging
-          movementLoop();
+          // console.log(i); //for debugging
+          playLoop();
       }
       break;
     case 'click':
       {
-        activeButton = 'movement';
-        movementLoop();
+        activeButton = 'play';
+        playLoop();
       }
       break;
     case 'stop':
       {
-        document.querySelector('.play').disabled = false;
-        document.querySelector('.next').disabled = false;
-        document.querySelector('.prev').disabled = false;
+        checkbox.disabled = false;
+        // console.log('stop'); //for debugging
       }
       break;
   }
 };
 
-movementLoop();
+playLoop();
 
 document.querySelector("#gallery .buttons .prev").onclick = function () {
-  activeButton = 'click';
+  if (activeButton !== 'stop') {
+    activeButton = 'click';
+  }
   imgArr[i].classList.remove('showed');
   i--;
   if (i < 0) {
     i = imgArr.length - 1;
   }
-  console.log(i); //for debugging
+  // console.log(i); //for debugging
   imgArr[i].classList.add('showed');
 };
 
-document.querySelector('.next').onclick = function () {
-  activeButton = 'click';
+document.querySelector('#gallery .buttons .next').onclick = function () {
+  if (activeButton !== 'stop') {
+    activeButton = 'click';
+  }
   imgArr[i].classList.remove('showed');
   i++;
   if (i >= imgArr.length) {
     i = 0;
   }
-  console.log(i); //for debugging
+  // console.log(i); //for debugging
   imgArr[i].classList.add('showed');
 };
 
-document.querySelector('.stop').onclick = function () {
-  activeButton = 'stop';
-  document.querySelector('.next').disabled = true;
-  document.querySelector('.prev').disabled = true;
-  this.disabled = true;
-  console.log('stop'); //for debugging
-};
-
-document.querySelector('.play').onclick = function () {
-  activeButton = 'movement';
-  this.disabled = true;
-  document.querySelector('.stop').disabled = false;
-  console.log('play'); //for debugging
-  movementLoop();
-};
+const checkbox = document.querySelector('#stop_play');
+checkbox.addEventListener('change', () => {
+  if (checkbox.checked) {
+    activeButton = 'play';
+    // console.log('play'); //for debugging
+    playLoop();
+  } else {
+    activeButton = 'stop';
+    checkbox.disabled = true;
+  }
+});
